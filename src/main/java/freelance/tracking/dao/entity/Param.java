b1b2b7a16f352032ef542994c5808b2eea9fc29b
Privatik3 <lol11999333@gmail.com>
2018-10-11 07:13:41 +0300

    Переработал AdInfo для добавления ярлыка new

---
+++ src/main/java/freelance/tracking/dao/entity/Param.java
@@ -0,0 +1,38 @@
+package freelance.tracking.dao.entity;
+
+public class Param {
+
+    public Param(String curVal) {
+        this.curVal = curVal;
+    }
+
+    private boolean change;
+    private String curVal;
+    private String prevVal;
+
+    private Status status;
+
+    protected void update(Param param) {
+        prevVal = param.curVal;
+        change = !curVal.equals(prevVal);
+
+        if (change)
+            updateStatus();
+    }
+
+    private void updateStatus() {
+        if (prepareVal(curVal) > prepareVal(prevVal))
+            status = Status.UP;
+        else
+            status = Status.DOWN;
+    }
+
+    private int prepareVal(String val) {
+        return Integer.parseInt(val.split("\\(")[0].replaceAll("\\s", ""));
+    }
+
+    public enum Status {
+        UP,
+        DOWN
+    }
+}
